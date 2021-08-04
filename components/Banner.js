141fba17eeb0d2161d79725385c9f41e5905a67f
Marlon Clay <luciousmc@icloud.com>
2021-08-04 14:32:23 -0700

    adds svg Logo adds header bg on scroll

--- components/Banner.js
+++ components/Banner.js
@@ -1,22 +1,25 @@
 import Image from 'next/image';
+import heroImage from '../images/hero-image.png';
 
 function Banner() {
   return (
-    <div className='relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]'>
+    <div className='relative h-[550px] sm:h-[650px]'>
       <Image
-        src='https://links.papareact.com/0fm'
+        src={heroImage}
         layout='fill'
         objectFit='cover'
       />
 
-      <div className='absolute top-1/2 w-full text-center'>
+      {/* <div className='absolute top-1/2 w-full text-center'>
         <p className='text-sm sm:text-lg'>Not sure where to go? Perfect.</p>
 
         <button className='text-purple-500 bg-white px-10 py-4 shadow-md rounded-full
         font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150'>
           I'm Flexible
         </button>
-      </div>
+      </div> */}
+
+
     </div>
   )
 }
--- components/Header.js
+++ components/Header.js
@@ -3,21 +3,46 @@ import { SearchIcon } from '@heroicons/react/solid';
 import { GlobeAltIcon } from '@heroicons/react/solid';
 import { MenuIcon } from '@heroicons/react/solid';
 import { UserCircleIcon } from '@heroicons/react/solid';
+import Logo from './Logo';
+import { useEffect, useRef, useState } from 'react';
 
 function Header() {
+  const headerRef = useRef();
+  const [fillHeader, setFillHeader] = useState(false);
+
+  const handleScroll = () => {
+    const { clientHeight } = headerRef.current;
+
+    if (window.pageYOffset > clientHeight) {
+      setFillHeader(true);
+    } else {
+      setFillHeader(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  },[]);
+
   return (
-    <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
-      <div className='relative flex items-center h-10 cursor-pointer'>
-        <Image
+    <header ref={headerRef} className={`fixed w-full top-0 z-50 grid grid-cols-3 bg-trasparent p-5 md:px-10 ${fillHeader ? 'bg-white transition duration-200 shadow-md ease-in-out' : ''}`}>
+      <div className='relative flex items-center h-10 cursor-pointer text-pink'>
+        {/* <Image
           src='https://links.papareact.com/qd3'
           alt='Logo'
           layout='fill'
           objectFit='contain'
           objectPosition='left'
-        />
+        /> */}
+        <Logo color={fillHeader ? 'text-pink-500' : 'text-white'} />
       </div>
       {/* Mid Section */}
       <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
+
         <input
           className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400'
           type="text"
@@ -28,12 +53,12 @@ function Header() {
 
       {/* Right Section */}
       <div className='flex space-x-4 items-center justify-end'>
-        <p className='hidden md:inline'>Become a host</p>
-        <GlobeAltIcon className='h-6' />
+        <p className='hidden md:inline text-gray-300'>Become a host</p>
+        <GlobeAltIcon className='h-6 text-gray-300' />
 
-        <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
-          <MenuIcon className='h-5' />
-          <UserCircleIcon className='h-6' />
+        <div className='flex items-center space-x-2 border-2 p-2 rounded-full bg-gray-200'>
+          <MenuIcon className='h-5 text-gray-600' />
+          <UserCircleIcon className='h-6 text-gray-600' />
         </div>
       </div>
     </header>
