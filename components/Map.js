60a07da23645e631d77cd8241f40d965ef4fdb91
Marlon Clay <luciousmc@icloud.com>
2021-08-06 09:26:55 -0700

    Mapbox API integrated

---
+++ components/Map.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
+import getCenter from 'geolib/es/getCenter';
+
+function Map({ searchResults }) {
+  const [viewport, setViewport] = useState({
+    width: '100%',
+    height: '100%',
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 11
+  });
+
+  const coords = searchResults.map(result => {
+    return {
+      longitude: result.long,
+      latitude: result.lat
+    }
+  });
+
+  console.log(coords)
+  
+  return (
+    <ReactMapGL
+      mapStyle='mapbox://styles/luciousmc/cks0ijw2654xv17t6yxxbp5eu'
+      mapboxApiAccessToken={process.env.mapbox_key}
+      {...viewport}
+      onViewportChange={(nextViewport) => setViewport(nextViewport)}
+    ></ReactMapGL>
+  )
+}
+
+export default Map;
--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ module.exports = {
     domains: [
       'links.papareact.com'
     ]
+  },
+  env: {
+    mapbox_key: 'pk.eyJ1IjoibHVjaW91c21jIiwiYSI6ImNrczBpMm0zcTFraHUydm4xMzg3d3ZvamQifQ.5zb28shX-aFSOQkdFDXTcg'
   }
 }
